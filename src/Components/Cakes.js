b87a02341848eb56b399e7fe2a1f341edbb41de1
Grzegorz <grzegorz.j.pawluk@gmail.com>
2021-04-07 20:21:20 +0200

    [Bakery] Images slider with splideJS
    Cakes images
    https://splidejs.com/

--- src/Components/Cakes.js
+++ src/Components/Cakes.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/themes/splide-skyblue.min.css';
+
 // import './../styles/style.css';
 import CakesStyles from './Cakes.module.css';
 import classes from 'classnames';
@@ -14,89 +17,150 @@ import cake1 from './../pictures/cake1.jpg';
 
 function Cakes() {
   return (
-    <div>
-      <h1>Ciasta</h1>
-      <div className={CakesStyles.gallery}>
-        <div
-          className={classes(
-            CakesStyles.transitionCake,
-            CakesStyles.fade,
-            CakesStyles.lesnyMech
-          )}
-        >
-          <img src={lesnyMech} alt="lesnyMech" />
-        </div>
-        <div
-          className={classes(
-            CakesStyles.transitionCake,
-            CakesStyles.fade,
-            CakesStyles.ketoTorcik
-          )}
-        >
-          <img src={ketoTorcik} alt="ketoTorcik" />
-        </div>
-        <div
-          className={classes(
-            CakesStyles.transitionCake,
-            CakesStyles.fade,
-            CakesStyles.batonBounty
-          )}
-        >
-          <img src={batonBounty} alt="batonBounty" />
-        </div>
-        <div
-          className={classes(
-            CakesStyles.transitionCake,
-            CakesStyles.fade,
-            CakesStyles.tortUrodzinowy
-          )}
-        >
-          <img src={tortUrodzinowy} alt="tortUrodzinowy" />
-        </div>
-        <div
-          className={classes(
-            CakesStyles.transitionCake,
-            CakesStyles.fade,
-            CakesStyles.tortMocnoCzekoladowy1
-          )}
-        >
-          <img src={tortMocnoCzekoladowy1} alt="tortMocnoCzekoladowy1" />
-          <img
-            className={CakesStyles.transitionImage}
-            src={tortMocnoCzekoladowy2}
-            alt="tortMocnoCzekoladowy2"
-          />
-        </div>
-        <div
-          className={classes(
-            CakesStyles.transitionCake,
-            CakesStyles.fade,
-            CakesStyles.hands
-          )}
-        >
-          <img src={hands} alt="hands" />
-        </div>
-        <div
-          className={classes(
-            CakesStyles.transitionCake,
-            CakesStyles.fade,
-            CakesStyles.dough
-          )}
-        >
-          <img src={dough} alt="dough" />
-        </div>
-        <div
-          className={classes(
-            CakesStyles.transitionCake,
-            CakesStyles.fade,
-            CakesStyles.cake1
-          )}
-        >
-          <img src={cake1} alt="cake1" />
-        </div>
-      </div>
+    <div className={CakesStyles.cake}>
+      <Splide
+        options={{
+          rewind: true,
+          gap: '1rem',
+        }}
+      >
+        <SplideSlide>
+          <div className={CakesStyles.cakeImage}>
+            <img src={lesnyMech} alt="lesnyMech" />
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div className={CakesStyles.cakeImage}>
+            <img src={ketoTorcik} alt="ketoTorcik" />
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div className={CakesStyles.cakeImage}>
+            <img src={batonBounty} alt="batonBounty" />
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div className={CakesStyles.cakeImage}>
+            <img src={tortUrodzinowy} alt="tortUrodzinowy" />
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div className={CakesStyles.cakeImage}>
+            <img src={tortMocnoCzekoladowy1} alt="tortMocnoCzekoladowy1" />
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div className={CakesStyles.cakeImage}>
+            <img src={tortMocnoCzekoladowy2} alt="tortMocnoCzekoladowy2" />
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div className={CakesStyles.cakeImage}>
+            <img src={hands} alt="hands" />
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div className={CakesStyles.cakeImage}>
+            <img src={dough} alt="dough" />
+          </div>
+        </SplideSlide>
+        <SplideSlide>
+          <div className={CakesStyles.cakeImage}>
+            <img src={cake1} alt="cake1" />
+          </div>
+        </SplideSlide>
+      </Splide>
     </div>
   );
 }
 
 export default Cakes;
+
+// function Cakes() {
+//   return (
+//     <div>
+//       <h1>Ciasta</h1>
+//       <div className={CakesStyles.gallery}>
+//         <div
+//           className={classes(
+//             CakesStyles.transitionCake,
+//             CakesStyles.fade,
+//             CakesStyles.lesnyMech
+//           )}
+//         >
+//           <img src={lesnyMech} alt="lesnyMech" />
+//         </div>
+//         <div
+//           className={classes(
+//             CakesStyles.transitionCake,
+//             CakesStyles.fade,
+//             CakesStyles.ketoTorcik
+//           )}
+//         >
+//           <img src={ketoTorcik} alt="ketoTorcik" />
+//         </div>
+//         <div
+//           className={classes(
+//             CakesStyles.transitionCake,
+//             CakesStyles.fade,
+//             CakesStyles.batonBounty
+//           )}
+//         >
+//           <img src={batonBounty} alt="batonBounty" />
+//         </div>
+//         <div
+//           className={classes(
+//             CakesStyles.transitionCake,
+//             CakesStyles.fade,
+//             CakesStyles.tortUrodzinowy
+//           )}
+//         >
+//           <img src={tortUrodzinowy} alt="tortUrodzinowy" />
+//         </div>
+//         <div
+//           className={classes(
+//             CakesStyles.transitionCake,
+//             CakesStyles.fade,
+//             CakesStyles.tortMocnoCzekoladowy1
+//           )}
+//         >
+//           <img src={tortMocnoCzekoladowy1} alt="tortMocnoCzekoladowy1" />
+//           <img
+//             className={CakesStyles.transitionImage}
+//             src={tortMocnoCzekoladowy2}
+//             alt="tortMocnoCzekoladowy2"
+//           />
+//         </div>
+//         <div
+//           className={classes(
+//             CakesStyles.transitionCake,
+//             CakesStyles.fade,
+//             CakesStyles.hands
+//           )}
+//         >
+//           <img src={hands} alt="hands" />
+//         </div>
+//         <div
+//           className={classes(
+//             CakesStyles.transitionCake,
+//             CakesStyles.fade,
+//             CakesStyles.dough
+//           )}
+//         >
+//           <img src={dough} alt="dough" />
+//         </div>
+//         <div
+//           className={classes(
+//             CakesStyles.transitionCake,
+//             CakesStyles.fade,
+//             CakesStyles.cake1
+//           )}
+//         >
+//           <img src={cake1} alt="cake1" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Cakes;
